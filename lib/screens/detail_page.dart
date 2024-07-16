469f351fc24748a809b2345d08054b2a728f5fe4
krisha kakadiya 4563 <144050204+krishakakadiya@users.noreply.github.com>
2024-07-16 16:41:39 +0530

    Update detail_page.dart

--- lib/screens/detail_page.dart
+++ lib/screens/detail_page.dart
@@ -10,15 +10,26 @@ class DetailPage extends StatefulWidget {
 
 class _DetailPageState extends State<DetailPage> {
   @override
-  bool isFav = false;
   Widget build(BuildContext context) {
     Size s = MediaQuery.of(context).size;
     Map<String, dynamic> detail =
         ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
     return Scaffold(
       appBar: AppBar(
-          // title:Text(),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).pushNamed('recipe_page');
+            },
+            icon: Icon(
+              Icons.restaurant_menu_outlined,
+              size: 30,
+              color: Colors.brown,
+            ),
           ),
+        ],
+        title: Text(detail['name']),
+      ),
       backgroundColor: Colors.brown.shade300,
       body: Padding(
         padding: const EdgeInsets.only(top: 16),
@@ -43,81 +54,97 @@ class _DetailPageState extends State<DetailPage> {
             ),
             Expanded(
               child: Container(
-                color: Colors.white,
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Expanded(
-                            child: Text(
-                              overflow: TextOverflow.ellipsis,
-                              detail['name'],
-                              style: const TextStyle(
-                                  fontSize: 30,
-                                  fontWeight: FontWeight.w500,
-                                  color: Colors.brown),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(20),
+                    topLeft: Radius.circular(20),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Expanded(
+                              child: Text(
+                                overflow: TextOverflow.ellipsis,
+                                detail['name'],
+                                style: const TextStyle(
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.brown),
+                              ),
                             ),
                           ),
-                        ),
-                        Expanded(
-                          child: IconButton(
-                            onPressed: () {
-                              if (!fav.contains(detail)) {
-                                fav.add(detail);
-                                setState(() {});
-                              } else {
-                                fav.remove(detail);
-                                setState(() {});
-                              }
-                            },
-                            icon: fav.contains(detail)
-                                ? const Icon(
-                                    Icons.favorite,
-                                    size: 30,
-                                    color: Colors.brown,
-                                  )
-                                : const Icon(
-                                    Icons.favorite_outline,
-                                    size: 30,
-                                    color: Colors.brown,
-                                  ),
+                          Expanded(
+                            child: IconButton(
+                              onPressed: () {
+                                if (!fav.contains(detail)) {
+                                  fav.add(detail);
+                                  setState(() {});
+                                } else {
+                                  fav.remove(detail);
+                                  setState(() {});
+                                }
+                              },
+                              icon: fav.contains(detail)
+                                  ? const Icon(
+                                      Icons.favorite,
+                                      size: 30,
+                                      color: Colors.brown,
+                                    )
+                                  : const Icon(
+                                      Icons.favorite_outline,
+                                      size: 30,
+                                      color: Colors.brown,
+                                    ),
+                            ),
                           ),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Expanded(
-                          child: IconButton(
-                            onPressed: () {
-                              if (!meal.contains(detail)) {
-                                meal.add(detail);
-                                setState(() {});
-                              } else {
-                                meal.remove(detail);
-                                setState(() {});
-                              }
-                            },
-                            icon: meal.contains(detail)
-                                ? const Icon(
-                                    Icons.no_meals_ouline,
-                                    size: 30,
-                                    color: Colors.brown,
-                                  )
-                                : const Icon(
-                                    Icons.set_meal,
-                                    size: 30,
-                                    color: Colors.brown,
-                                  ),
+                          SizedBox(
+                            width: 10,
                           ),
+                          Expanded(
+                            child: IconButton(
+                              onPressed: () {
+                                if (!meal.contains(detail)) {
+                                  meal.add(detail);
+                                  setState(() {});
+                                } else {
+                                  meal.remove(detail);
+                                  setState(() {});
+                                }
+                              },
+                              icon: meal.contains(detail)
+                                  ? const Icon(
+                                      Icons.no_meals_ouline,
+                                      size: 30,
+                                      color: Colors.brown,
+                                    )
+                                  : const Icon(
+                                      Icons.set_meal,
+                                      size: 30,
+                                      color: Colors.brown,
+                                    ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Column(
+                          children: [],
                         ),
-                      ],
-                    ),
-                    const Column(
-                      children: [],
-                    ),
-                  ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),

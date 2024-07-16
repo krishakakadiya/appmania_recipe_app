2d64c87bdbaf9a21bac5fd125e824502e521975c
krisha kakadiya 4563 <144050204+krishakakadiya@users.noreply.github.com>
2024-07-16 16:41:23 +0530

    Update home_page.dart

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -10,7 +10,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  bool isFav = false;
   @override
   Widget build(BuildContext context) {
     Size s = MediaQuery.of(context).size;
